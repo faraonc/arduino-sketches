e59c3dce35a1f99ebd9a86b1793d09f9e89720b0
faraonc <faraonc@outlook.com>
2017-10-05 21:44:28 -0800

    Finished Lab2 ex1,2.

---
+++ sketch_oct05a_lab2_1/sketch_oct05a_lab2_1.ino
@@ -0,0 +1,25 @@
+/*
+ * Conard James B. Faraon
+ * Pouria Ghadimi
+ * CSS427 Fall 2017 Lab2 Ex1
+ * Board used: Arduino Micro
+ */
+
+const int PHOTOR_PIN = A2;
+int lightLevel;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(PHOTOR_PIN, INPUT);
+    lightLevel = 0;
+}
+
+void loop()
+{
+   lightLevel = analogRead(PHOTOR_PIN);
+   Serial.println(lightLevel);
+
+   //slow down the transmission for effective Serial communication.
+   delay(50);
+}
---
+++ sketch_sep29a_lab1_10/sketch_oct05a_lab2_2/sketch_oct05a_lab2_2.ino
@@ -0,0 +1,38 @@
+/*
+ * Conard James B. Faraon
+ * Pouria Ghadimi
+ * CSS427 Fall 2017 Lab2 Ex2
+ * Board used: Arduino Micro
+ */
+ 
+const int LED_PIN = 2; //D2
+const int PHOTOR_PIN = 4; //D4
+int ledState;
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(PHOTOR_PIN, INPUT);
+    ledState = 0;
+}
+
+void loop()
+{
+   ledState = digitalRead(PHOTOR_PIN);
+   Serial.println("ledState = " + String(ledState));
+
+   if(ledState == 1)  //1 is ON
+   {
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("Status: LED ON");
+   }
+   else   //0 is OFF
+   {
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("Status: LED OFF");
+   }
+
+   //slow down the transmission for effective Serial communication.
+   delay(50);
+}

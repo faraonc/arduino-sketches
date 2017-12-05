b2fa712f784d8d2b9df6c403c28cce3aa5b825c6
faraonc <faraonc@outlook.com>
2017-12-05 14:05:55 -0800

    Test AJAX with Connection, closed and disabling interrupts. Test AltSoft.

--- webpage/smdb.js
+++ webpage/smdb.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
 	$('#jumbox').css("background-color", "#4b2e83").css("color", "#fff");
 	$('#db-header').text("Smart Doorbell Dashboard");
 	$('hr').css("border-top", "1px solid #85754d");
-	$('#foot').text("\u00a9 2017 Copyright: Pouria & Conard");
+	$('#foot').text("\u00a9 2017 Copyright: UWB CSS 427 Fall 2017");
 	$('.col-sm-4:nth-child(1)').css("color", "black"); //alt #a6a6a6
 	$('.col-sm-4:nth-child(2)').css("color", "black");
 	$('.col-sm-4:nth-child(3)').css("color", "black");
